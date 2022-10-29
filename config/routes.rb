13a45b1dd2c8556e1a6089e7c94ebdcbd07122a8
Nan <nanliao917@gmail.com>
2022-10-29 13:08:58 -0700

    outside of app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rottenpotatoes::Application.routes.draw do
-  resources :movies
+  resources :ingredients
   # map '/' to be a redirect to '/movies'
-  root :to => redirect('/movies')
+  root :to => redirect('/ingredients')
 end
