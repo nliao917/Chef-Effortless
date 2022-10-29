ba0d7fb45a74721453ec4b386582bd9172145eda
JenniferChenCU <91161902+JenniferChenCU@users.noreply.github.com>
2022-10-29 16:36:59 -0400

    Untrack files in .gitignore

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,14 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
 
-ingredients = [{:item => 'Potato', :quantity => '1', :expiration_date => 'Seven days later'},
-          {:item => 'Tomato', :quantity => '1', :expiration_date => 'Seven days later'},
-          {:item => 'Rice', :quantity => '1', :expiration_date => 'Seven days later'},
-          {:item => 'Beef', :quantity => '1', :expiration_date => 'Seven days later'},
-          {:item => 'Tofu', :quantity => '1', :expiration_date => 'Seven days later'},
-          {:item => 'Cheese', :quantity => '1', :expiration_date => 'Seven days later'},
-          {:item => 'Bread', :quantity => '1', :expiration_date => 'Seven days later'},
-          {:item => 'Egg', :quantity => '1', :expiration_date => 'Seven days later'}
+ingredients = [{:item => 'Potato', :quantity => '1', :expiration_date => DateTime.new(2009,9,1,17)},
+          {:item => 'Tomato', :quantity => '1', :expiration_date => DateTime.new(2022,12,9,17)},
+          {:item => 'Rice', :quantity => '1', :expiration_date => DateTime.new(2022,9,11,13)},
+          {:item => 'Beef', :quantity => '1', :expiration_date => DateTime.new(2022,10,12,17)},
+          {:item => 'Tofu', :quantity => '1', :expiration_date => DateTime.new(2022,11,1,17)},
+          {:item => 'Cheese', :quantity => '1', :expiration_date => DateTime.new(2022,12,1,15)},
+          {:item => 'Bread', :quantity => '1', :expiration_date => DateTime.new(2022,11,1,8)},
+          {:item => 'Egg', :quantity => '1', :expiration_date => DateTime.new(2023,1,12,17)}
   	 ]
 
 ingredients.each do |ingredient|
