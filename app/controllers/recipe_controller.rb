00fe1ed8521baae49700e32673e805123649f43b
JenniferChenCU <91161902+JenniferChenCU@users.noreply.github.com>
2022-11-09 18:13:36 -0500

    add simple rank func

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -1,5 +1,6 @@
 class RecipeController < ApplicationController
-  @@apiKey = "7f274a56343748968771ed0642f79c6c"
+  # @@apiKey = "7f274a56343748968771ed0642f79c6c"
+  @@apiKey = "2e644af47d964b57a970710eec23c5bb"
 
   def index
     @recipes =[]
@@ -12,7 +13,8 @@ class RecipeController < ApplicationController
 
     objs = get_recipes
     objs.each do |obj|
-      @recipes.push(obj['title'])
+      # @recipes.push(obj['title'])
+      @recipes.push(obj)
     end
     
   end
@@ -21,19 +23,94 @@ class RecipeController < ApplicationController
     params.require(:recipe)
   end
 
+  def score(recipe)
+    # 80% ingredients
+    # occassion, diets, cuisines, readyTime
+    # default score, 
+    # 
+    cuisine = recipe["cuisines"]
+    diet = recipe["diets"]
+    # intolerance = recipe
+    occasions = recipe["occasions"]
+    readyTime = recipe["readyInMinutes"]
+    
+    final_score = 0
+    if not params["recipe"]["cuisine"] == "Any"
+      if recipe["cuisines"].include?(params["recipe"]["cuisine"])
+        final_score += 1
+      else
+        final_score -= 1
+      end
+    end
+
+    if not params["recipe"]["diet"] == "Any"
+      if recipe["diets"].include?(params["recipe"]["diets"])
+        final_score += 1
+      else
+        final_score -= 1
+      end
+    end
+      
+    # if not params["recipe"]["intolerances"] == "None"
+    #   if recipe["intolerances"].include?(params["recipe"]["intolerances"])
+    #     final_score += 1
+    #   else
+    #     final_score -= 1
+    #   end
+    # end
+      
+    if not params["recipe"]["occasion"] == "Any"
+      if recipe["occasions"].include?(params["recipe"]["occasion"])
+        final_score += 1
+      else
+        final_score -= 1
+      end
+    end
+
+    if not params["recipe"]["time"] == "Any"
+      if recipe["readyInMinutes"]<=params["recipe"]["time"].split()[1].to_i()
+        final_score += 1
+      else
+        final_score -= 1
+      end
+    end
+    return final_score
+
+  end
+
+
   def call_service(items)
     ss = items.map do |item|
       item.item
     end
+
     # print(params['recipe'])
+    if not params.keys().include?("recipe")
+      params["recipe"]={"cuisine"=>"Any", "diet"=>"Any", "intolerances"=>"None", "occasion"=>"Any", "time"=>"Any"}
+    end
 
     key = ss.join(',')
     num = 5
     site = "https://api.spoonacular.com/recipes/findByIngredients"
     objs = HTTParty.get(site, {query: {ingredients: key, number: num, apiKey: @@apiKey}, header: {'Content-Type' => 'application/json'}}).parsed_response
 
-    return objs 
+    recipe_list = []
 
+
+    # more info about objs
+    objs.each do |obj|
+      info = HTTParty.get("https://api.spoonacular.com/recipes/"+obj['id'].to_s+"/information", {query: {apiKey: @@apiKey}}) 
+    #   # score ranking list display
+    #   # ready_time = info["readyInMinutes"]
+      recipe_list.append([info["title"], info["cuisines"], info["diets"], info["occasions"], info["readyInMinutes"], score(info)])
+    #   # puts(info.keys().include?("score"))
+    #   # obj["score"] = score(info)
+    #   # add score as key
+    #   obj["score"] = score(info)
+    end
+
+
+    return recipe_list
   end
 
 
