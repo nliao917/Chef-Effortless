1286b6b947876926e4b04f891b1e8eb7d54370da
JenniferChenCU <91161902+JenniferChenCU@users.noreply.github.com>
2022-11-09 22:53:50 -0500

    Revert "iter2 first"
    This reverts commit 891f9ee3d5fc7381aeae067521e826b3d769007a.

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -1,18 +1,5 @@
-require "json"
-require "base64"
-
-
 class RecipeController < ApplicationController
   @@apiKey = "7f274a56343748968771ed0642f79c6c"
-  @@tags = [:cuisine, :intolerances, :diet, :type]
-
-
-  def show
-    puts "show"
-    puts params
-
-    @obj = load_recipe(params[:title])
-  end
 
   def index
     @recipes =[]
@@ -22,54 +9,16 @@ class RecipeController < ApplicationController
     #if Ingredient.find_quantity('Beef') >0 and  Ingredient.find_quantity('Potato') >0
     #  @recipes.push("Beef over Potato")
     #end
-    @filters = {}
-    @@tags.each do | item |
-      puts item
-      @filters[item] = 'Any'
-    end
-
-    puts @filters
-
 
     objs = get_recipes
     objs.each do |obj|
-      #puts obj
-
-      #save_recipe(obj)
-      puts obj
-
-      @recipes.push(obj)
+      @recipes.push(obj['title'])
     end
     
   end
 
   def recipe_params
     params.require(:recipe)
-  end
-
-  def load_recipe(title) 
-    obj = nil
-    File.open(title, 'r') do | f |
-      content = f.read
-      obj = JSON.parse(content)
-    end
-
-    return obj
-  end
-
-  def save_recipes(objs)
-    objs.each do | obj |
-      save_recipe(obj)
-    end
-  end
-
-  def save_recipe(obj) 
-    title = obj['title']
-    title = Base64.encode64(title)
-    content = obj.to_json
-    File.open(title, 'w') do | f |
-      f.write(content)
-    end
   end
 
   def call_service(items)
@@ -80,24 +29,8 @@ class RecipeController < ApplicationController
     num = 5
     site = "https://api.spoonacular.com/recipes/findByIngredients"
 
-    query_param = {ingredients: key, number: num, apiKey: @@apiKey}
-    #condition_param = params[:condition]
-    #if condition_param
-    #  @@tags.each do | item |
-    #    v = condition_param[item]
-    #    if v && v != 'Any'
-    #      puts v
-    #      @filters[item] = v
-    #      query_param[item] = v
-    #    end
-    #  end
-    #end
 
-    puts query_param
-
-
-
-    objs = HTTParty.get(site, {query: query_param, header: {'Content-Type' => 'application/json'}}).parsed_response
+    objs = HTTParty.get(site, {query: {ingredients: key, number: num, apiKey: @@apiKey}, header: {'Content-Type' => 'application/json'}}).parsed_response
 
     return objs 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rottenpotatoes::Application.routes.draw do
   get 'recipe/index'
-  get 'recipe/show/:title', to: 'recipe#show', as: 'recipe'
 
   resources :ingredients
   root to: "welcome#index"
