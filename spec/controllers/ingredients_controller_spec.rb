3f2f5dab3a28d96d3e9eee62231ad2a67162c53d
JenniferChenCU <91161902+JenniferChenCU@users.noreply.github.com>
2022-12-12 17:33:51 -0500

    db table

--- spec/controllers/ingredients_controller_spec.rb
+++ spec/controllers/ingredients_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe IngredientsController, type: :controller do
 
     describe "Post #create" do
       it "redirect to index page" do
-        post :create, {ingredient: {item: 'test', quantity: 2, expiration__date: '2023-05-25'}}
+        post :create, {ingredient: {item: 'test', quantity: 2, expiration__date: '2023-05-25', user_id: '1'}}
         expect(response).to redirect_to(ingredients_path)
       end
     end
